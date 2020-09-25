aec7454879ea7bb24bf679bcb0ff141b8af69d09
Your Name <Kitti@gmail.com>
2020-09-26 01:19:57 +0500

    Commneting Code

--- React Class work/firstreactproject/src/App.js
+++ React Class work/firstreactproject/src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-    return ( <
-        div >
-        <
-        h1 > Hello < /h1> < /
-        div >
-    );
-}
 
-// class App extends React.Component {
-//     render() {
-//         let firstname = "Usama";
-//         let lasttname = "Rajput";
-//         let arr = [1,2,3,4];
-//         return ( 
-//             <div>
-//             <h1> {`${firstname} ${lasttname}`} < /h1>
-// <h1>{arr}</h1>
-//             </div >
-//         )
-//     }
+// Function Component
+// ******************
+
+// function App() {
+//     return ( <
+//         div >
+//         <
+//         h1 > Hello < /h1> < /
+//         div >
+//     );
 // }
+
+
+// Class Component
+// ******************
+class App extends React.Component {
+    render() {
+        let firstname = "Usama";
+        let lasttname = "Rajput";
+        let arr = [1, 2, 3, 4];
+        return ( <
+            div >
+            <
+            /div >
+        )
+    }
+}
 
 export default App;
