df2dbf70f2edc88134eb459b23f7e0b57cdf3d2e
Your Name <Kitti@gmail.com>
2020-09-26 01:09:52 +0500

    Using FUnction Componenet

--- React Class work/firstreactproject/src/App.js
+++ React Class work/firstreactproject/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import './App.css';
 
-// function App() {
-//     return ( <
-//         div className = "App" >
-
-//         <
-//         h1 > Hello < /h1> < /
-//         div >
-//     );
-// }
-
-class App extends React.Component {
-    render() {
-        let firstname = "Usama";
-        let lasttname = "Rajput";
-        return ( 
-            <div>
-            <h1> {`${firstname} ${lasttname}`} < /h1> 
-            </div >
-        )
-    }
+function App() {
+    return ( <
+        div >
+        <
+        h1 > Hello < /h1> < /
+        div >
+    );
 }
+
+// class App extends React.Component {
+//     render() {
+//         let firstname = "Usama";
+//         let lasttname = "Rajput";
+//         let arr = [1,2,3,4];
+//         return ( 
+//             <div>
+//             <h1> {`${firstname} ${lasttname}`} < /h1>
+// <h1>{arr}</h1>
+//             </div >
+//         )
+//     }
+// }
 
 export default App;
