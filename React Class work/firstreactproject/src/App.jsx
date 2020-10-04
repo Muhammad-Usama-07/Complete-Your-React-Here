78fd09b5ff23a28fa329d4ecfe7d0ee48f059f56
Your Name <Kitti@gmail.com>
2020-10-04 20:24:59 +0500

    Commenting code

--- React Class work/firstreactproject/src/App.jsx
+++ React Class work/firstreactproject/src/App.jsx
@@ -55,54 +55,54 @@ class App extends React.Component {
         }
     }
     
-    set_name = () => {
-        this.setState({
-            name: this.state.value
-        })
-    }
-    get_name = () => {
-        console.log(this.state.name)
-    }
-    handleChange(e){
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-    get_prop = (props) =>{
-        console.log(props)
-    }
+    // set_name = () => {
+    //     this.setState({
+    //         name: this.state.value
+    //     })
+    // }
+    // get_name = () => {
+    //     console.log(this.state.name)
+    // }
+    // handleChange(e){
+    //     this.setState({
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
+    // get_prop = (props) =>{
+    //     console.log(props)
+    // }
     render() {
        // let todos = [{ text: "ghous" }, { text: "basit" }];
         return (
-            <div> 
-                <Logo />
-                <Header get_prop = {this.get_prop} name = {this.state.name} email={this.state.email} page="Application Page" />
-                <h2>My name is {this.state.name}</h2>
-                <h4>My email is: {this.state.email}</h4>
-                <input name="name" onChange = {(e) => this.handleChange(e)} type="text" placeholder= "name here"></input>
-                <input name="email" onChange = {(e) => this.handleChange(e)} type="text" placeholder= " email"></input>
+            // <div> 
+            //     <Logo />
+            //     <Header get_prop = {this.get_prop} name = {this.state.name} email={this.state.email} page="Application Page" />
+            //     <h2>My name is {this.state.name}</h2>
+            //     <h4>My email is: {this.state.email}</h4>
+            //     <input name="name" onChange = {(e) => this.handleChange(e)} type="text" placeholder= "name here"></input>
+            //     <input name="email" onChange = {(e) => this.handleChange(e)} type="text" placeholder= " email"></input>
 
-                <button onClick = {this.set_name}>set name</button>
-                <button onClick = {this.get_name}>get name</button>
+            //     <button onClick = {this.set_name}>set name</button>
+            //     <button onClick = {this.get_name}>get name</button>
 
-            <h1 style = {{color: 'red'}}>Main Component </h1>
-                <br />
-                <BasicTextFields />
-                <br />
-                <button type="button" className="btn btn-primary">Primary</button>
+            // <h1 style = {{color: 'red'}}>Main Component </h1>
+            //     <br />
+            //     <BasicTextFields />
+            //     <br />
+            //     <button type="button" className="btn btn-primary">Primary</button>
             
             
-            {
-                /*<
-                            ul > {
-                                todos.map((item, index) => {
-                                    return <li key = { index } > { item.text } < /li>
-                                })
-                            } <
-                            /ul>*/
-            } 
-                <Footer />
-            </div >
+            // {
+            //     /*<
+            //                 ul > {
+            //                     todos.map((item, index) => {
+            //                         return <li key = { index } > { item.text } < /li>
+            //                     })
+            //                 } <
+            //                 /ul>*/
+            // } 
+            //     <Footer />
+            // </div >
         )
     }
 }
