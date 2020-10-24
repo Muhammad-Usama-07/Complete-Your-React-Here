d7546ce46fde6e0683f0772ad778cab2a8a59fc1
Your Name <Kitti@gmail.com>
2020-10-24 19:22:05 +0500

    Export header and footer component

--- React Projects/FirstProject/myfirstproject/src/components/Footer.js
+++ React Projects/FirstProject/myfirstproject/src/components/Footer.js
@@ -4,9 +4,10 @@ import React, {Component} from 'react';
 class Footer extends Component{
     render(){
         return(
-        <div>
+        <div className = "Footer">
             <h1>Footer</h1>
         </div>
         )
     }
 }
+export default Footer;
--- React Projects/FirstProject/myfirstproject/src/components/Header.js
+++ React Projects/FirstProject/myfirstproject/src/components/Header.js
@@ -4,9 +4,10 @@ import React, {Component} from 'react';
 class Hooter extends Component{
     render(){
         return(
-        <div>
+        <div className = "Header">
             <h1>Header</h1>
         </div>
         )
     }
 }
+export default Header;
