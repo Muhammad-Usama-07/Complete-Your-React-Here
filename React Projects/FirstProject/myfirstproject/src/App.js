6463fb5a45823150e043f8a06945a598bdff43b8
Your Name <Kitti@gmail.com>
2020-10-24 19:31:45 +0500

    Calling header and footer component

--- React Projects/FirstProject/myfirstproject/src/App.js
+++ React Projects/FirstProject/myfirstproject/src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
     render(){
         return(
             <div>
-            <h1>Nevbar</h1>
+            <Header />
+            <h1>Main Body</h1>
+            <Footer />
             </div>
             
         )
