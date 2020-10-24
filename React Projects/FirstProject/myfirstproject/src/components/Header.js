594e0771d4dbd5c2d0cb75cfdf117d67b2a38e98
Your Name <Kitti@gmail.com>
2020-10-24 19:19:08 +0500

    Creating Class Component in header and footer

--- React Projects/FirstProject/myfirstproject/src/components/Header.js
+++ React Projects/FirstProject/myfirstproject/src/components/Header.js
@@ -1,3 +1,12 @@
 import React, {Component} from 'react';
 
 
+class Hooter extends Component{
+    render(){
+        return(
+        <div>
+            <h1>Header</h1>
+        </div>
+        )
+    }
+}
