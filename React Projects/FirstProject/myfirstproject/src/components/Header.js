8d1b70794410d4ccc5b7ad51502d0a2a20bf66a1
Your Name <Kitti@gmail.com>
2020-10-30 22:11:58 +0500

    Working Header.js

--- React Projects/FirstProject/myfirstproject/src/components/Header.js
+++ React Projects/FirstProject/myfirstproject/src/components/Header.js
@@ -4,9 +4,25 @@ function Header() {
     return ( 
         <div className = "header fixed flex" >
             <div className = "logo" >
-            <img src = { require("../Images/olxlogo.png") }/> 
+            <img src = { require("../Images/olxlogo.png") } />  
             </div> 
-        </div>
+            <div className = "location rel" >
+                <div className = "icon-search" / >
+                <input className = "label" placeholder = "your Location" / >
+                <button className = "icon-chevron-down" /> 
+            </div>  
+            <div className="search aic">
+                <input type="text" placeholder = "Find Cars, Mobile phones and more..." className="query" / > <button className = "icon-search" /> 
+  
+            </div>
+            <div className="action flex aic">
+                <button className = "">
+                    <div className = "icon-plus" />
+                        <h2 className="">Sell</h2>
+                </button> 
+  
+            </div>
+        < /div>
     )
 }
 
