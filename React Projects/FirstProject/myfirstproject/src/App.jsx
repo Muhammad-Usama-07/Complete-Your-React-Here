296b6bd5f0f7b169ea2c38fff937e022a199bb73
Your Name <Kitti@gmail.com>
2020-10-30 15:28:48 +0500

    importing Header and HomePage

--- React Projects/FirstProject/myfirstproject/src/App.jsx
+++ React Projects/FirstProject/myfirstproject/src/App.jsx
@@ -1,11 +1,13 @@
 import React, {Component}from 'react';
 import './App.css';
 import {BrowserRouter, Route,Link} from 'react-router-dom'
-
+import Header from './components/Header'
+import HomePage form './components/Home'
 
 
 function App() {
         return(
+            <Header></Header>
             <BrowserRouter>
                 <Route path = '/'></Route>
             </BrowserRouter>
