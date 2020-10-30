c5783068f44ae07f76315033ae4c561ea231df41
Your Name <Kitti@gmail.com>
2020-10-30 22:23:52 +0500

    Linking classes woth css

--- React Projects/FirstProject/myfirstproject/src/components/Header.js
+++ React Projects/FirstProject/myfirstproject/src/components/Header.js
@@ -7,18 +7,18 @@ function Header() {
             <img src = { require("../Images/olxlogo.png") } />  
             </div> 
             <div className = "location rel" >
-                <div className = "icon-search" / >
-                <input className = "label" placeholder = "your Location" / >
-                <button className = "icon-chevron-down" /> 
+                <div className = "icon-search ico" / >
+                <input className = "label s15 font" placeholder = "your Location" / >
+                <button className = "icon-chevron-down arro" /> 
             </div>  
             <div className="search aic">
-                <input type="text" placeholder = "Find Cars, Mobile phones and more..." className="query" / > <button className = "icon-search" /> 
+                <input type="text" placeholder = "Find Cars, Mobile phones and more..." className="query font s15" / > <button className = "icon-search go" /> 
   
             </div>
-            <div className="action flex aic">
+            <div className="actions flex aic">
                 <button className = "">
                     <div className = "icon-plus" />
-                        <h2 className="">Sell</h2>
+                        <h2 className="s15 font">Sell</h2>
                 </button> 
   
             </div>
